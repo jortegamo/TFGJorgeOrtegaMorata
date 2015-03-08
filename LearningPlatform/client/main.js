c0fe6f30dfbdb430d24e34e9493b414bd01499d4
Jorge Ortega Morata <j.ortega.morata@gmail.com>
2015-03-08 19:38:45 +0100

    añadida plantilla pagina de inicio, reestructuración código en events al estilo meteor y creada aplicación en SoundCloud y enlazada al iniciar el cliente mediante script en main.html

---
+++ LearningPlatform/client/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	//inicializamos el cliente de SoundCloud.
+	SC.initialize({
+		client_id: "1adb9624dcfef41b1b0b1f45adc6fa7c"
+	});
+});
--- LearningPlatform/client/templates/postSubmit.js
+++ LearningPlatform/client/templates/postSubmit.js
@@ -1,48 +1,16 @@
-Template.postSubmit.rendered = function(){
-	var editor = ace.edit("editor");
-	editor.setTheme("ace/theme/twilight");
-    editor.getSession().setMode("ace/mode/javascript");
+//variable global para el elemento ACE.editor.
+var editor;
 
-    //variables de sesión para establecer la configuración multimedia.
-    Session.set("cam-enabled",false);
-    Session.set("mic-enabled",true);
-
-    $('#config-editor-button').popover({
-    	content: Blaze.toHTML(Template.configEditor),
-        html: true,
-        template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
-    	trigger: "click",
-    	placement: "right"
-    });
-
-    $('#config-editor-button').click(function(){
-        $("#themes").click(function(){
-            $(".theme").click(function(){
-                editor.setTheme("ace/theme/" + this.text);
-            });
-        });
-        $("#modes").click(function(){
-            $(".mode").click(function(){
-                editor.getSession().setMode("ace/mode/" + this.text);
-            })
-        });
-    });
-
+Template.postSubmit.events = {
     
-
-    $('#multmedia-config').popover({
-    	content: Blaze.toHTML(Template.configMultimedia),
-        html: true,
-    	trigger: "click",
-    	placement: "top"
-    });
-
-    $('#multmedia-config').click(function(){
+    'click #multmedia-config': function(){
         $("#micro-button").click(function(){
             if ($(this).hasClass('btn-success')){
                 $(this).removeClass('btn-success').addClass('btn-danger');
+                //disabled micro
                 Session.set("mic-enabled",false);
             }else{
+                //enabled micro
                 $(this).removeClass('btn-danger').addClass('btn-success');
                 Session.set("mic-enabled",true);
             }
@@ -51,18 +19,68 @@ Template.postSubmit.rendered = function(){
         $("#cam-button").click(function(){
             if ($(this).hasClass('btn-success')){
                 $(this).removeClass('btn-success').addClass('btn-danger');
+                //disabled cam
                 Session.set("cam-enabled",false);
             }else{
                 $(this).removeClass('btn-danger').addClass('btn-success');
+                //enabled cam
                 Session.set("cam-enabled",true);
             }
         });
+    },
+
+    'click #config-editor-button': function(){
+        $("#themes").click(function(){
+            $(".theme").click(function(){
+                editor.setTheme("ace/theme/" + this.text);
+            });
+        });
+
+        $("#modes").click(function(){
+            $(".mode").click(function(){
+                editor.getSession().setMode("ace/mode/" + this.text);
+            })
+        });
+    }
+
+};
+
+Template.postSubmit.rendered = function(){
+	editor = ace.edit("editor");
+	editor.setTheme("ace/theme/twilight");
+    editor.getSession().setMode("ace/mode/javascript");
+
+    //variables de sesión para establecer la configuración multimedia.
+    Session.set("cam-enabled",false);
+    Session.set("mic-enabled",true);
+
+    //generamos los popovers Bootstrap para que muestren los paneles de configuración.
+    //panel configuración editor
+    $('#config-editor-button').popover({
+        content: Blaze.toHTML(Template.configEditor),
+        html: true,
+        title: "Editor Options",
+        template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
+        trigger: "click",
+        placement: "right"
     });
 
-    $('#help-button').popover({
-    	content: Blaze.toHTML(Template.heopInfoRecord),
-    	html: true,
+    //panel configuracion multimedia
+    $('#multmedia-config').popover({
+        content: Blaze.toHTML(Template.configMultimedia),
+        html: true,
         trigger: "click",
-    	placement: "right"
+        placement: "top"
     });
-}
+
+    //panel ayuda
+    $('#help-button').popover({
+        content: Blaze.toHTML(Template.heopInfoRecord),
+        html: true,
+        trigger: "click",
+        placement: "right"
+    });
+
+};
+
+
