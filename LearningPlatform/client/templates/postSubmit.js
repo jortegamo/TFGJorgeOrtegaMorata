9e37a63ea785000cb0f5a15b97f578b17f6e4fa4
Jorge Ortega Morata <j.ortega.morata@gmail.com>
2015-03-16 19:36:38 +0100

    creados objetos toDo identificados a una marca de tiempo. Probando idea. Con PopCorn.js se hará más facil. Mejorados eventos del editor

--- LearningPlatform/client/templates/postSubmit.js
+++ LearningPlatform/client/templates/postSubmit.js
@@ -1,6 +1,5 @@
 //variable global para el elemento ACE.editor.
 editor = "";
-
 Template.postSubmit.events = {
     
     'click #multmedia-config': function(){
@@ -41,6 +40,18 @@ Template.postSubmit.events = {
                 editor.getSession().setMode("ace/mode/" + this.text);
             })
         });
+    },
+
+    'click #record-button': function(){
+        if (Session.get("recording")){
+            Session.set("recording",false);
+            play();
+        }else{
+            console.log("comienzo la grabacion");
+            Session.set("recording", true);
+            date = new Date();
+            date2 = date;
+        }
     }
 
 };
@@ -53,43 +64,104 @@ Template.postSubmit.rendered = function(){
     editor2 = ace.edit("editor2");
     editor2.setTheme("ace/theme/twilight");
     editor2.getSession().setMode("ace/mode/javascript");
-    editor2.setReadOnly(true);
+    //editor2.setReadOnly(true);
     editor2.focus();
     //vamos a probar los eventos del editor y tratamos de reproducirlos en el segundo editor.
     //eventos
-
+    functions = [];
     editor.getSession().on('change', function(e) {
+        
         switch (e.data.action){
             case "removeText":
                 var rmRange = e.data.range;
                 var doc = editor2.getSession().getDocument();
                 doc.remove(rmRange);
+                functions.push({
+                    time: new Date() - date,
+                    interval: new Date() -date2,
+                    toDo: (function(){
+                        return function(editor){
+                            editor.getSession().getDocument().remove(rmRange);
+                        };
+                    }())
+                });
+                date2 = new Date();
                 break;
             case "insertText":
                 editor2.insert(e.data.text);
+                functions.push({
+                    time: new Date() - date,
+                    interval: new Date() -date2,
+                    toDo: (function(){
+                        return function(editor){
+                            editor.insert(e.data.text);
+                        };
+                    }())
+                });
+                date2 = new Date();
                 break;
             case "removeLines":
                 var rmRange = e.data.range;
                 var doc = editor2.getSession().getDocument();
                 doc.remove(rmRange);
+                functions.push({
+                    time: new Date() - date,
+                    interval: new Date() -date2,
+                    toDo: (function(){
+                        return function(editor){
+                            editor.getSession().getDocument().remove(rmRange);
+                        };
+                    }())
+                });
+                date2 = new Date();
                 break;
-        }   
+        } 
     });
 
-    editor.getSession().selection.on('changeCursor', function(e) {
+    editor.getSession().selection.on('changeSelection', function(e) {
         var selection = editor.getSession().selection;
         var selection2 = editor2.getSession().selection;
-        if(!selection.isEmpty()){
-            console.log("he entrado en el correcto");
-            var cursor = selection.getCursor();
-            selection2.moveCursorTo(cursor.row,cursor.column);
-            var range = selection.getRange();
-            selection2.selectTo(range.start.row,range.start.column);
-        }else{
+        
+        if(selection.isEmpty()){
             selection2.clearSelection();
-            var cursor = selection.getCursor();
-            selection2.moveCursorTo(cursor.row,cursor.column);
+            functions.push({
+                time: new Date() - date,
+                interval: new Date() -date2,
+                toDo: (function(){
+                    return function(editor){
+                        editor.getSession().selection.clearSelection();
+                    };
+                }())
+            });
+        }else{
+            var range = selection.getRange();
+            selection2.setRange(range);
+            functions.push({
+                time: new Date() - date,
+                interval: new Date() -date2,
+                toDo: (function(){
+                    return function(editor){
+                        editor.getSession().selection.setRange(range);
+                    };
+                }())
+            });
         }
+        date2 = new Date();
+    });
+
+    editor.getSession().selection.on('changeCursor',function(e){
+        var pos = editor.getCursorPosition();
+        editor2.moveCursorToPosition(pos);
+        functions.push({
+            time: new Date() - date,
+            interval: new Date() -date2,
+            toDo: (function(){
+                return function(editor){
+                    editor.moveCursorToPosition(pos);
+                }
+            }())
+        });
+        date2 = new Date();
     });
     
 
@@ -107,7 +179,7 @@ Template.postSubmit.rendered = function(){
     //variables de sesión para establecer la configuración multimedia.
     Session.set("cam-enabled",false);
     Session.set("mic-enabled",true);
-
+    Session.set("recording",false);
     //generamos los popovers Bootstrap para que muestren los paneles de configuración.
     //panel configuración editor
     $('#config-editor-button').popover({
@@ -136,5 +208,33 @@ Template.postSubmit.rendered = function(){
     });
 
 };
+
+//funcion que relaiza la reproduccion de la grabacion en el editor.
+var play = function(){
+    console.log("he llamado a play");
+    editor2.setValue("");
+    console.log("he reseteado el editor2");
+    var times = [];
+    _(functions).each(function(elem){
+        times.push(elem.time);
+    })
+    times = _(times).uniq();
+    console.log("uniq times: ");
+    console.log(times);
+    var reproduction = [];
+    _(times).each(function(time){
+        var elems = _(functions).filter(function(elem){return elem.time == time;});
+        console.log(elems);
+        reproduction.push(elems);
+    });
+    console.log("reproduction");
+    console.log(reproduction);
+    _(reproduction).each(function(elems){
+            _(elems).each(function(elem){
+                elem.toDo(editor2);
+            });
+    });
+    console.log("he terminado la reproduccion");
+}
 
 
